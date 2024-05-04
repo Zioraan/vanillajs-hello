6dd987cec38e3fa1db7f151ff447f3e72f973d88
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-05-04 22:06:41 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -6,6 +6,61 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+let generateExcuse = () => {
+  let who = [
+    "My cat",
+    "My Dog",
+    "The mailman",
+    "My girlfriend",
+    "My spouse",
+    "My coworker",
+    "The janitor",
+    "A clown"
+  ];
+  let action = [
+    " ate",
+    " destroyed",
+    " took",
+    " ripped",
+    " broke",
+    " crushed",
+    " hid",
+    " stole"
+  ];
+  let what = [
+    " my homework",
+    " my car",
+    " my house",
+    " my computer",
+    " my grandma",
+    " my hamster",
+    " my pencil",
+    " my water heater"
+  ];
+  let when = [
+    " last night",
+    " this morning",
+    " before my shift",
+    " before I woke up",
+    " as I was driving",
+    " yesterday",
+    " at noon",
+    " before I left"
+  ];
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindex] +
+    "" +
+    action[actionindex] +
+    "" +
+    what[whatindex] +
+    "" +
+    when[whenindex]
+  );
 };
